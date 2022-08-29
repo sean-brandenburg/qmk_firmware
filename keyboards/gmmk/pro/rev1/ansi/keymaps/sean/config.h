3bb2d89f76a2e2209063a5b352c36e9925834934
sean-brandenburg <seanbrandenburg333@gmail.com>
2022-08-28 22:52:09 -0400

    custom gmmk pro config

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/sean/config.h
@@ -0,0 +1,6 @@
+/* Change polling rate to 1000hz and read more keys per scan for lower input latency */
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
+#define RGBLIGHT_SLEEP
+#define RGB_DISABLE_TIMEOUT 180000 // milliseconds
+#define RGB_DISABLE_WHEN_USB_SUSPENDED 1
